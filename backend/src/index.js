0a5b516a61dbdbddfe1fa4446eb735def758ed55
Juan Vitor <juandolino8@gmail.com>
2020-05-02 21:17:57 -0300

    Adicionado CORS.

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,11 @@ const express = require("express");
 
 const routes = require("./routes");
 
+const cors = require("cors");
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
