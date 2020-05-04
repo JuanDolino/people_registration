d52ae5e43a3ec5f717d460558fb2f60843546de8
Juan Vitor <juandolino8@gmail.com>
2020-05-04 19:09:45 -0300

    Integração do back-end com o front-end completa.

--- backend/src/database/controllers/PeopleController.js
+++ backend/src/database/controllers/PeopleController.js
@@ -10,17 +10,9 @@ module.exports = {
   },
 
   async index(req, res) {
-    const { page = 1 } = req.query;
-
-    const [ count ] = await connection("people").count();
-
     const people = await connection
-    .limit(5)
-    .offset((page - 1) * 5)
     .select("*")
     .from("people");
-
-    res.header("X-Total-Count", count['count(*)']);
 
     return res.json({people});
   },
--- frontend/src/pages/NewPeople/index.js
+++ frontend/src/pages/NewPeople/index.js
@@ -8,9 +8,22 @@ import { FiPlus, FiArrowLeft } from 'react-icons/fi';
 
 import { Link } from 'react-router-dom';
  
+import api from '../../services/api';
+
 import './styles.css';
 
 function NewPeople() {
+  async function handleAddPeople() {
+    const name = document.querySelector(".input-name").value;
+
+    const age = document.querySelector(".input-age").value;
+    
+    await api.post("people", {
+      name,
+      age
+    })
+  }
+
   return (
     <>
       <Header/>
@@ -21,13 +34,13 @@ function NewPeople() {
         <div>
           <h1>Register People</h1>
           <div className="group-input">
-            <input type="text" placeholder="Name:"/>
-            <input type="text" placeholder="Age:"/>
+            <input type="text" placeholder="Name:" className="input-name"/>
+            <input type="text" placeholder="Age:" className="input-age"/>
           </div>
 
           <div className="group-button">
             <button><Link to="/"><FiArrowLeft style={{ width: "20px", height: "20px" }} color={"#6F66D8"} />Back to home</Link></button>
-            <button><Link to="/"><FiPlus style={{ width: "20px", height: "20px" }} color={"#6F66D8"}/>Add new people</Link></button>
+            <button><Link to="/" onClick={handleAddPeople}><FiPlus style={{ width: "20px", height: "20px" }} color={"#6F66D8"}/>Add new people</Link></button>
           </div>
 
         </div>
